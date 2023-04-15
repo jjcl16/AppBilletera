e61174e0b827caa212d04e3fbaeac25e1f271905
jjcl16 <46204792+jjcl16@users.noreply.github.com>
2023-04-15 10:51:39 -0300

    Update processor.js
    change OPTIONS request to a GET request (method)

--- processor.js
+++ processor.js
@@ -21,7 +21,7 @@ function readQR (){
 
 async function logJSONData(url) {
   const response = await fetch(url , {
-    method: "OPTIONS",
+    method: "GET",
     credentials: 'include',
     mode: "cors",
 		headers: {
@@ -29,8 +29,8 @@ async function logJSONData(url) {
       'Access-Control-Allow-Credentials': true,
       'Access-Control-Allow-Methods': 'GET',
       'Access-Control-Allow-Headers': 'Content-Type',
-			'apikey': '21Vfcf1ywAIA4tFXEhOh3bsojGWujfvZ',
-			'Host': 'developers.decidir.com',
+      'apikey': '21Vfcf1ywAIA4tFXEhOh3bsojGWujfvZ',
+      'Host': 'developers.decidir.com',
       'Origin':'developers.decidir.com',
       'Cookie' : 'TS013d7616=012e46d8ee6b6eb80bb529584c0dab1e369bb23840f70d368fd8c520cc753c11a09eea1a6fb1de45a22170cb216f2e901e43530c66; TS017a11a6=012e46d8ee327a922c53dc0e133290f37628ef1726bb01169b16a1aa6d29c5c2c8d03b8dd5e4bb6b7c5d17895694b14b34d60020ad'
 		},
